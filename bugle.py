66c387395a7808ed96894f6b72a991b046677e83
correctunicycle <54482145+correctunicycle@users.noreply.github.com>
2022-07-20 03:06:32 +0100

    added user path selection and combined for loops

--- bugle.py
+++ bugle.py
@@ -1,26 +1,27 @@
 import urllib.request
-
-for x in range(1,10):
-    
-    url = 'http://thebugle.leekworld.com/thebugle00'+ str(x) + '.mp3'
-    urllib.request.urlretrieve(url, 'insert path for file storage here, example path shown in the other for loops will also need to be replaced with your path'+ str(x) + '.mp3')
-    print('downloading: ')
-    print(url)
-
-
-for x in range(10,100):
-    
-    url = 'http://thebugle.leekworld.com/thebugle0'+ str(x) + '.mp3'
-    urllib.request.urlretrieve(url, '/Users/john/Documents/TimesOnlineBugle/bugle0'+ str(x) + '.mp3')
-    print('downloading: ')
-    print(url)
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+path = askdirectory(title='Select Folder') # shows dialog box and return the path
+print(path) 
+for x in range(1,179):
+    if x < 10:
+        url = f'http://thebugle.leekworld.com/thebugle00{x}.mp3'
+        urllib.request.urlretrieve(url, f'{path}/thebugle00{x}.mp3')
+        print('downloading: ')
+        print(url)
 
 
+    if x >9 and x <100: 
+        url = f'http://thebugle.leekworld.com/thebugle0{x}.mp3'
+        urllib.request.urlretrieve(url, f'{path}/thebugle0{x}.mp3')
+        print('downloading: ')
+        print(url)
 
 
-for x in range(100,179):
-    
-    url = 'http://thebugle.leekworld.com/thebugle'+ str(x) + '.mp3'
-    urllib.request.urlretrieve(url, '/Users/andy/Documents/TimesOnlineBugle/bugle'+ str(x) + '.mp3')
-    print('downloading: ')
-    print(url)
+
+
+    if x > 100 and x<179:    
+        url = f'http://thebugle.leekworld.com/thebugle0{x}.mp3'
+        urllib.request.urlretrieve(url, f'{path}/thebugle0{x}.mp3')
+        print('downloading: ')
+        print(url)
